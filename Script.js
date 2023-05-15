033e7fa895c3fc9ba3cfa49510ee967097e4324a
Sotravil <78595995+Sotravil@users.noreply.github.com>
2023-05-15 12:10:59 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -41,7 +41,41 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
   });
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {document.addEventListener('DOMContentLoaded', function() {
+  const taskInput = document.getElementById('taskInput');
+  const addButton = document.getElementById('addButton');
+  const taskList = document.getElementById('taskList');
+
+  addButton.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+      const listItem = document.createElement('li');
+      listItem.textContent = taskText;
+
+      listItem.addEventListener('click', function() {
+        listItem.classList.toggle('completed');
+      });
+
+      listItem.addEventListener('dblclick', function() {
+        listItem.remove();
+      });
+
+      taskList.appendChild(listItem);
+      taskInput.value = '';
+    }
+  });
+
+  taskInput.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault(); // Prevent form submission
+      addButton.click();
+    }
+  });
+});
+
   const taskInput = document.getElementById('taskInput');
   const addButton = document.getElementById('addButton');
   const taskList = document.getElementById('taskList');
