fcb95450381f57a2bc23eb5b3bfe6129a860c45a
Sotravil <78595995+Sotravil@users.noreply.github.com>
2023-05-15 11:55:33 -0500

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  const taskInput = document.getElementById('taskInput');
+
+  const addButton = document.getElementById('addButton');
+
+  const taskList = document.getElementById('taskList');
+
+  // Add task event
+
+  addButton.addEventListener('click', function() {
+
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+
+      const listItem = document.createElement('li');
+
+      listItem.textContent = taskText;
+
+      // Mark task as completed on click
+
+      listItem.addEventListener('click', function() {
+
+        listItem.classList.toggle('completed');
+
+      });
+
+      // Remove task on double click
+
+      listItem.addEventListener('dblclick', function() {
+
+        listItem.remove();
+
+      });
+
+      taskList.appendChild(listItem);
+
+      taskInput.value = '';
+
+    }
+
+  });
+
+  // Add task on pressing Enter key
+
+  taskInput.addEventListener('keypress', function(event) {
+
+    if (event.key === 'Enter') {
+
+      addButton.click();
+
+    }
+
+  });
+
+});
+
